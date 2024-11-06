282b80bfb63214feac799598895191f0e6497ccc
Vincenzo Scognamiglio <vincenzo.scognamiglio2@unina.it>
2024-11-06 15:15:43 +0100

    added file for t265 in ROS

--- ov_msckf/launch/rs_launch.py
+++ ov_msckf/launch/rs_launch.py
@@ -22,8 +22,8 @@ from launch.substitutions import LaunchConfiguration
 
 
 configurable_parameters = [{'name': 'camera_name',                  'default': 'camera', 'description': 'camera unique name'},
-                           {'name': 'camera_namespace',             'default': 'd435i', 'description': 'namespace for camera'},
-                           {'name': 'serial_no',                    'default': "'213222078674'", 'description': 'choose device by serial number'},
+                           {'name': 'camera_namespace',             'default': '', 'description': 'namespace for camera'},
+                           {'name': 'serial_no',                    'default': "", 'description': 'choose device by serial number'},
                            {'name': 'usb_port_id',                  'default': "''", 'description': 'choose device by usb port id'},
                            {'name': 'device_type',                  'default': "''", 'description': 'choose device by type'},
                            {'name': 'config_file',                  'default': "''", 'description': 'yaml config file'},
